3b8f95e2ce724d0d00ca1bfcff92bd9dcc63924f
Muhammad Aamir <aemistar635@gmail.com>
2019-09-03 11:33:22 +0500

    hy

--- common/sql_connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$db_link = mysqli_connect('localhost','root','','phone_book');
-	session_start();
-	
-	$userId = $_SESSION['user_id'] ?? "";
-    $userRoll = $_SESSION['user_roll'] ?? "";
-
-?>
--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset($_SESSION['user_id']);
-session_unset($_SESSION['user_role']);
-session_destroy();
-header('location:login.php');
-?>
